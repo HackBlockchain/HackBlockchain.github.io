f1febf78e9767af8192bee6e711d7985130f72c6
Shannon Foster <ShannonWie@users.noreply.github.com>
2016-11-22 18:31:55 -0500

    Add files via upload
    Updated image locations

---
+++ promo.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".fullscreen-static-image1").backstretch(["Site/assets/img/HackBlockchain.png"],
+  {duration: 10000, fade:1500}),
+  $(".fullscreen-static-image2").backstretch(["Site/assets/img/edit.jpg"],
+  {duration: 10000, fade:1500});
+});
+
+// , "../../img/HackBlockchain.png"
+// , "../../img/HackBlockchain.png"
